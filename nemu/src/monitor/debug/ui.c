76e852e3bfce2c943fa99b85306ef4fc9971a333
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 08:10:24 +0000

    >  run 3024244103 zzz Linux 4a9995a92c1b 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:10:24 up 22 min,  0 users,  load average: 0.01, 0.02, 0.00 b3f142df4983bc8895f0287359dfa05e255b222

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,50 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_info(char *args) {
+	int i;
+	for (i=0;i<8;i++){
+
+		switch (i){
+			case 0:
+			printf("eax %x",cpu.gpr[0]._32);
+			break;
+			case 1:
+			printf("ecx %x",cpu.gpr[1]._32);
+			break;
+			case 2:
+			printf("edx %x",cpu.gpr[2]._32);
+			break;
+			case 3:
+			printf("ebx %x",cpu.gpr[3]._32);
+			break;
+			case 4:
+			printf("esp %x",cpu.gpr[4]._32);
+			break;
+			case 5:
+			printf("ebp %x",cpu.gpr[5]._32);
+			break;
+			case 6:
+			printf("esi %x",cpu.gpr[6]._32);
+			break;
+			case 7:
+			printf("edi %x",cpu.gpr[7]._32);
+			break;
+		}
+	}
+	return 0;
+}
+static int cmd_step(char *args){
+	cpu_exec(1);
+	return 0;
+}
+static int cmd_x(char *args){
+	int N;
+	uint32_t EXPR;
+	sscanf(args,"x %d %x",&N,&EXPR);
+	swaddr_read(EXPR,N);
+	return 0;
+}
 static struct {
 	char *name;
 	char *description;
@@ -48,6 +92,11 @@ static struct {
 	{ "q", "Exit NEMU", cmd_q },
 
 	/* TODO: Add more commands */
+	{"step", "step one instruction", cmd_step},
+	{"info r", "Print register state", cmd_info},
+	{"step", "step one instruction", cmd_step},
+	{"x", "examine 内存", cmd_x},
+
 
 };
 
