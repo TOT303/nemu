fd73c82d319b94c59ecce71e8b95552c97e5efee
zhang junchuan <in28461937@163.com>
2025-09-29 18:23:18 +0800

    pa2

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -7,4 +7,14 @@ make_helper(ret) {
     print_asm("ret");
     return 0; 
 }
+
+make_helper(ret_i) {
+	uint16_t imm = instr_fetch(eip + 1, 2);
+	cpu.eip = swaddr_read(cpu.esp, 4) - (2 + 1);
+	cpu.esp += 4 + imm;
+
+	print_asm("ret $0x%x", imm);
+
+	return 2 + 1;
+}
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/ret.h
+++ nemu/src/cpu/exec/control/ret.h
@@ -3,6 +3,6 @@
 
 
 make_helper(ret);
-
+make_helper(ret_i);
 
 #endif
